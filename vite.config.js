8436d3ec2a4e9bbbe162f375b9edd3bf6762d963
Leileisme <onlymymind1108@gmail.com>
2025-02-10 20:50:34 +0800

    fax:CI/CD部署文件

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === 'production' ? '/REact-2024-week5/' : '/',
+  base:process.env.NODE_ENV === 'production' ? '/React-2024-week5/' : '/',
   plugins: [react()],
 })
